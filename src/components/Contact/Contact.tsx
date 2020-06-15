81352e73258a530135ca4892496767328eac9bd3
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-06-15 01:16:12 -0300

    Updated Styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,7 +5,11 @@ import { Input, ContentInput, Button } from './styles';
 const Contact: React.FC = () => {
   return (
     <Row className="justify-content-center">
-      <Col xs={12} lg={{ size: 8, offset: 2 }} className="text-center">
+      <Col
+        xs={12}
+        lg={{ size: 8, offset: 0 }}
+        className="text-center text-lg-left"
+      >
         <ContentInput className="align-items-center justify-content-around">
           <Col xs={12} lg="auto" className="text-center text-lg-left">
             <Input
@@ -14,8 +18,8 @@ const Contact: React.FC = () => {
               autoCapitalize="false"
             />
           </Col>
-          <Col xs={12} lg="auto" className="text-center">
-            <Button>Enviar</Button>
+          <Col xs={12} lg="auto" className="text-center py-3 pt-lg-0">
+            <Button className="w-100 w-lg-auto ">Enviar</Button>
           </Col>
         </ContentInput>
       </Col>
