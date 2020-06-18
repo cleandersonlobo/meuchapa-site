bc2f3abe2a5b2ecdc0cd8509bd8d42dff2d93387
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-06-18 11:41:14 -0300

    Updated no layout das features

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -8,50 +8,50 @@ import { Circle, Title } from './styles';
 
 const Features: React.FC = () => {
   return (
-    <Row className="flex-lg-row-reverse align-items-center">
-      <Col xs={12} lg={6}>
+    <Row className="flex-lg-row-reverse align-items-center justify-content-start">
+      <Col xs={12} lg={6} className="text-center text-lg-left">
         <Row className="align-items-center pb-5">
-          <Col xs="auto">
+          <Col xs={12} lg="auto">
             <Circle color="#51CDA2">
               <GiHeartBeats size={35} color="#ffffff" />
             </Circle>
           </Col>
-          <Col>
+          <Col xs={12} lg="auto">
             <Title>Saúde Mental</Title>
           </Col>
         </Row>
-        <Row className="align-items-center pb-5">
-          <Col xs="auto">
+        <Row className="align-items-center pb-5 pl-0 pl-lg-5">
+          <Col xs={12} lg="auto">
             <Circle color="#F67062">
               <IoMdHappy size={35} color="#ffffff" />
             </Circle>
           </Col>
-          <Col>
+          <Col xs={12} lg="auto">
             <Title>Amigável</Title>
           </Col>
         </Row>
-        <Row className="align-items-center pb-5">
-          <Col xs="auto">
+        <Row className="align-items-center pb-5 pl-0 pl-lg-5">
+          <Col xs={12} lg="auto" className="text-center">
             <Circle color="#5049E6">
               <AiFillTrophy size={35} color="#ffffff" />
             </Circle>
           </Col>
-          <Col>
+          <Col xs={12} lg="auto">
             <Title>Gamificação</Title>
           </Col>
         </Row>
         <Row className="align-items-center pb-5">
-          <Col xs="auto">
+          <Col xs={12} lg="auto">
             <Circle color="#19147a">
               <FcServices size={35} color="#fff" />
             </Circle>
           </Col>
-          <Col>
+          <Col xs={12} lg={6}>
             <Title>Serviços do Caminhoneiro</Title>
           </Col>
         </Row>
       </Col>
-      <Col xs={12} lg="auto">
+      <Col xs={12} lg={6} className="text-center text-lg-left">
         <img
           className="img-fluid"
           src="assets/images/Grupo102.png"
--- src/components/Features/styles.ts
+++ src/components/Features/styles.ts
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 export const Circle = styled.div`
   padding: 20px;
+  display: inline-block;
   border-radius: 50px;
+  width: 75px;
+  height: 75px;
+  text-align: center;
   background-color: ${(props) => props.color || '#eeeeee'};
 `;
 
